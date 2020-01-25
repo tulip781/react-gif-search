053fd5a0dba8db326af80b53356be38d1d2299e8
Oliver Inglis <ollie@theinglistwins.com>
2020-01-25 17:53:17 +0000

    Need to add current Gif Selection"

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
 
 class App extends Component {
-  render() {
-    const gifs = [
-      { id: '7MF21rP5CsIpy' },
-      { id: '26mE6kbZ8e4aWuZ2w' }
-    ];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      gifs: [],
+      selectedGifId: "7MF21rP5CsIpy"
+    };
+  }
+
+  search = (query) => {
+    giphy('cMMs0WeW3ygpUaT5523WSX5cbFXsPvTI').search({
+      q: query,
+      rating: 'g'
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  currentGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar search={this.search} />
           <div className="selected-gif">
-          <Gif id="7MF21rP5CsIpy" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={this.state.gifs}/>
+          <GifList gifs={this.state.gifs} currentGifFunction={this.currentGif} />
         </div>
       </div>
     );
--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 import Gif from './gif.jsx';
 
 class GifList extends Component {
-  renderList = () => {
-    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} />);
+  updater = () => {
+    console.log(this.props.currentGifFunction(this.id));
   }
+
+  renderList = () => {
+    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} onClick={this.updater} />);
+  }
+
 
   render() {
     return (
